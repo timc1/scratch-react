2a192346965882fe4d8c8041fed492af7cc39e40
Tim <timchang.tcc@gmail.com>
2019-01-15 12:12:13 -0800

    Add react

--- src/components/some-component/test.js
+++
@@ -1,8 +0,0 @@
-import { square } from '../../utils'
-
-export default function Test() {
-  const squared = square(3)
-  const el = document.createElement('h1')
-  el.innerText = squared
-  return el
-}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
-import './shared/style.scss'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app'
 
-function component() {
-  let el = document.createElement('button')
-  el.innerText = 'click to lazy load'
-  el.addEventListener('click', e => {
-    import(/* webpackChunkName: 'printme' */ './components/some-component/index').then(
-      mod => {
-        const printMe = mod.default
-
-        printMe().then(element => {
-          document.body.appendChild(element)
-        })
-      }
-    )
-  })
-
-  return el
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  let element = component()
-  console.log(element)
-  document.body.appendChild(element)
-
-  if (module.hot) {
-    module.hot.accept('./components/some-component/index', function() {
-      console.log('Accepting the updated printMe module!')
-      document.body.removeChild(element)
-      element = component()
-      document.body.appendChild(element)
-    })
-  }
-}
+ReactDOM.render(<App />, document.getElementById('___root'))
